0537424795f9d57f31259302449237a52cfa5d77
Paul Miller <paul.miller@gmail.com>
2016-07-31 11:31:56 -0700

    bootstrep from http://www.hanselman.com/blog/CommentView.aspx?guid=E7CA62F5-2B81-471E-A027-1373AE58887A#f8701c2b-a47b-4878-9547-e3308c7d3589

---
+++ HomeModule.cs
@@ -0,0 +1,12 @@
+namespace britney
+{
+    using Nancy;
+
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", _ => "Hello World"); 
+        }
+    }
+}
